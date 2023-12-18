9e0091ec8514cee251b324751498798a2a141ac1
Tyas Oetomo <nie.tyaz@gmail.com>
2023-12-19 03:19:25 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,26 +53,31 @@ function searchCity(city)  {
 function displayForecast() {
     let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
     let forecastHtml = "";
-
+  
     days.forEach(function (day) {
-        focecastHtml +
-        '
+      forecastHtml =
+        forecastHtml +
+        `
         <div class="weather-forecast-day">
           <div class="weather-forecast-date">${day}</div>
           <div class="weather-forecast-icon">🌤️</div>
-          <div class="weather-forecast-temperature">
-            <strong>15°</strong>
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>15º</strong>
+            </div>
+            <div class="weather-forecast-temperature">9º</div>
           </div>
-          <div class="weather-forecast-temperature">9°</div>
         </div>
-    </div>
-
-    ';
-    
+      `;
     });
+  
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+  }
 
 
     let searchFormElement = document.querySelector("#search-form");
     searchFormElement.addEventListener("submit", handleSearchSuubmit);
   
-    searchCity("Bandung")
+    searchCity("Bandung");
+    displayForecast();
