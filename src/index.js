a3b94fd25fed73988c99c3c431e4b100adeb4b49
Tyas Oetomo <nie.tyaz@gmail.com>
2023-12-19 17:14:16 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -77,15 +77,15 @@ function displayForecast(response) {
         forecastHtml +
         `
          <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${formatDay(day.time)}></div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
          <div class="weather-forecast-icon"></div>
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
-            <strong>${Math.round(day.temperature.maximum)}>°</strong>
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
             </div>
             <div class="weather-forecast-temperature">${Math.round(
               day.temperature.minimum
-            )}>°</div>
+            )}°</div>
           </div>
         </div>
       `;
